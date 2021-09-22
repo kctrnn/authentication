48db9bdcdbc1c94428f5ac2b628de204b8d4e227
kctrnn <kctrnn@gmail.com>
2021-09-22 12:24:09 +0700

    Update private route

--- src/components/Common/PrivateRoute.js
+++ src/components/Common/PrivateRoute.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 
-const PrivateRoute = (props) => {
+const PrivateRoute = ({ children, ...rest }) => {
   // Check if user is logged in
   // If yes, show route
   // Otherwise, redirect to login page
 
   const isLoggedIn = Boolean(localStorage.getItem("access_token"));
-  if (!isLoggedIn) return <Redirect to='/auth' />;
 
-  return <Route {...props} />;
+  // if (!isLoggedIn) return <Redirect to='/auth' />;
+
+  // return <Route {...props} />;
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        return isLoggedIn ? children : <Redirect to='/auth' />;
+      }}
+    />
+  );
 };
 
 export default PrivateRoute;
